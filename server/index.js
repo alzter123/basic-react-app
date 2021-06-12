0bf9a4d362712830a6f56e18ef18e417f4971ade
Aldrich Nasayao <alzterbengtson@gmail.com>
2021-06-12 12:55:16 +0800

    The full CRUD (no design) version

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-const express = require("express");
+const express = require('express');
+const app = express(); //MANDATORY: MUST ALWAYS BE AFTER THE EXPRESSJS DECLARATION
 const cors = require("cors");
 const bodyParser = require('body-parser');
 // const bodyParser = require("body-parser");
-const app = express();
+
 //NOTE: express can be declared many times. One for main (app), one for other things like admin, employees, clients, etc. with their own separate variables
 const mysql = require("mysql");
 
@@ -15,30 +16,68 @@ const db = mysql.createPool({
 
 app.use(cors());
 app.use(bodyParser.json()) //THIS IS THE MIDDLEWARE
-app.use(express.json);
+
+app.get('/', (req, res) => {
+    res.send("Hello world!");
+});
+
+app.get('/api/get', (req, res) => { //the res here is important to load to tthe frontend/DOM
+ 
+    const sqlDisplay = "SELECT * FROM characters";
+    db.query(sqlDisplay, (error, result) => { //the res here is the result of the query
+        var objectified = result.map((mysqlObj, index) => { //THIS CODE IS USED TO TURN THE ROWDATAPACKET INTO ARRAY OBJECTS
+            return Object.assign({}, mysqlObj);  //
+        })
+        var newResponse = Object.assign({}, objectified); //I DECLARED THE ARRAY OBJECT TO ANOTHER VARIABLE FOR CLEANER CALL
+        res.send(newResponse);
+      //res.send(newResponse);
+    });
+}); 
 
 app.post('/api/insert', (req, res) => {    // SLASH MEANS DEFAULT FOR API ROUTING PAGE LOCATION. THIS IS LIKE HAVING API INTERACT
     //WITH ONLY THE PAGE SET TO THE SERVER. THIS IS KINDA GOOD FOR REUSING API PLUS PAGE HANDLING AS WELL BASED FOM MY OBSERVATION.
 //    console.log(req);
-   
+
 //     //CONNECT TO DATABASE TEST
 //     // const sqlInsert = "INSERT INTO characters (CharacterName) VALUES ('Finn the Human');";
 //     // db.query(sqlInsert, (req, res) => {
 //     //     res.send("HELLO!");
 //     //     console.log(res);
 //     // });
-console.log(req.body);
+
     const characterName = req.body.characterName; 
-    console.log("WORKS!");
     const sqlInsert = "INSERT INTO characters (CharacterName) VALUES (?)"; //THIS IS HOW TO INJECT DATA TO SQL USING REACT EXPRESS
-    db.query(sqlInsert, [characterName], (err, res) => {
-        res.send(res);
+    db.query(sqlInsert, [characterName], (error, result) => {
+        res.send(result);
+    });
+
+});
+
+app.delete('/api/delete/:characterId', (req, res) => {   // :nameOfPassedParam => this is the format (like in Vue Routing, right?)
+
+    const characterId = req.params.characterId; 
+    const sqlDelete = "DELETE FROM characters WHERE CharacterId = ?"; //THIS IS HOW TO INJECT DATA TO SQL USING REACT EXPRESS
+    db.query(sqlDelete, [characterId], (error, result) => {
+        res.send(result);
+    });
+
+});
+
+app.put('/api/update/:characterId', (req, res) => {   // :nameOfPassedParam => this is the format (like in Vue Routing, right?)
+    
+    const updateCharacterId = req.params.characterId; 
+    const updateCharacterName = req.body.newCharacterName; 
+    const sqlUpdate = "UPDATE characters SET CharacterName = ? WHERE CharacterId = ?"; //THIS IS HOW TO INJECT DATA TO SQL USING REACT EXPRESS
+    db.query(sqlUpdate, [updateCharacterName, updateCharacterId], (error, result) => {
+        //console.log(result);
+        res.send(result);
     });
 
 });
 
 
 //NOTE USE taskkill /F /IM node.exe to reset the port back to 3000
+//ADDED NOTE FOR API & PORT RELATIONSHIP: SO BASICALLY, WHATEVER NEW PORT WE ARE GOING TO RUN, IT AHS TO BE THE FETCH API'S PORT TOO
 app.listen(3001, () => {
-    console.log("running at port 3001")
+    console.log("running at port 3001") 
 })
--- src/App.js
+++ src/App.js
@@ -1,40 +1,128 @@
-import React, { useState } from 'react'
+// import axios from 'axios';
+import React, { useState, useEffect } from 'react'
 import './App.css';
-import axios from 'axios'
-//import AddCharacters from './AddCharacters.js';
 
 function App() { 
-
+ //NOTE FOR setSomethings: This is for having the method for any changes to the 'Something' variable's values.
   const [characterName, setCharacterName] = useState("");
-  const options = {
-    method: "POST",
-    headers: { 'content-type':'application/json'},
-    data: JSON.stringify({characterName: characterName})
-  }
-  const submitCharacter = () => {
-    console.log(characterName);
-    axios.post('http://localhost:3001/api/insert', options).then(response => {
-      console.log(response); 
-      alert("SUCCESSFUL INSERT!");
-    }).catch(error => {
-      console.log(error);
-    })
+  const [characterList, setCharacterList] = useState([]);
+  const [newCharacterName, setNewCharacterName] = useState("");
+
+  
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type':'application/json',
+      }
+    }
+  
+
+    fetch('http://127.0.0.1:3001/api/get', options) //THE URL FORMAT: SINCE 3000 IS DEFAULT, USE 3001 TO HANDLE API TO THE
+    //ROUTER AS WELL AS USE THE 127.0.0.1 ACCORDING TO KUYA FOR BETTER COMMUNICATION TO THE SERVER
+    .then(response => response.json()) //Converted ARRAY OBJECT TO JUST OBJECT
+    .then(response2 => Object.values(response2)) //Convert OBJECT TO ONLY ARRAY so it can be mapped to the browser
+    .then(response3 => setCharacterList(response3)) //DECLARE THE ARRAY TO THE CHARACTERLIST USING SETCHARACTERLIST
+    .catch(err => console.log(err));
+    
+  }, []);
+
+  const submitCharacter = (e) => {
+    e.preventDefault(); //USE THIS TO AVOID REFRESH AFTER ACTION
+    //BELOW IS KUYA'S CODE, THIS CAN STILL BE OPTIMIZED FOR BETTER RENDERING SUCH AS USING AXIOS NSTEAD OF FETCH, ETC.
+   const options = {
+      method: 'POST',
+      body: JSON.stringify({characterName: characterName}),
+      headers: {
+        'Content-Type':'application/json',
+      }
+    }
+  
+
+    fetch('http://127.0.0.1:3001/api/insert', options) //THE URL FORMAT: SINCE 3000 IS DEFAULT, USE 3001 TO HANDLE API TO THE
+    //ROUTER AS WELL AS USE THE 127.0.0.1 ACCORDING TO KUYA FOR BETTER COMMUNICATION TO THE SERVER
+    .then(response => response.json())
+    .then(result => setTimeout(function(){ 
+      
+      window.location.reload();
+      alert("Character successfully added!");  
+      console.log(result.data)
+      
+    }, 500)
+    )
+    .then(result2 => setCharacterList(result2.data))
+    .catch(err => console.log(err));
   };
 
+  const deleteHandler = (idVal) => {
+    //newValue.preventDefault();
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type':'application/json',
+      }
+    }
+
+    fetch(`http://127.0.0.1:3001/api/delete/${idVal}`, options) //THE URL FORMAT: SINCE 3000 IS DEFAULT, USE 3001 TO HANDLE API TO THE
+    //ROUTER AS WELL AS USE THE 127.0.0.1 ACCORDING TO KUYA FOR BETTER COMMUNICATION TO THE SERVER
+    .then(response => 
+      setTimeout(function(){ 
+        alert("Character successfully deleted!"); 
+        window.location.reload(); 
+      }, 3000))
+      .catch(err => console.log(err));
+  };
+
+  const updateHandler = (idVal) => {
+    const options = {
+      method: 'PUT',
+      body: JSON.stringify({newCharacterName: newCharacterName}),
+      headers: {
+        'Content-Type':'application/json',
+      }
+    }
+    setNewCharacterName("");
+    fetch(`http://127.0.0.1:3001/api/update/${idVal}`, options) //THE URL FORMAT: SINCE 3000 IS DEFAULT, USE 3001 TO HANDLE API TO THE
+    //ROUTER AS WELL AS USE THE 127.0.0.1 ACCORDING TO KUYA FOR BETTER COMMUNICATION TO THE SERVER
+    .then(response => 
+      setTimeout(function(){ 
+        alert("Character successfully updated!"); 
+        window.location.reload(); 
+      }, 500)
+    ).catch(err => console.log(err));
+  };
+
+
+  //FOR BUTTON, USE TYPE: BUTTON INSTEAD OF SUBMIT
   return (  
     <div className="App">
-      {/* <AddCharacters /> */}
-      
-      <form>
+    
                 <label><h1>Add A Character</h1></label>
                 <input type="text" name="characterName" onChange = {(e) => {
                   setCharacterName(e.target.value)
                 }} />
                 <br/>
                 <br/>
-                <button type="submit" onClick={ submitCharacter }>Submit</button>
-   
-            </form>
+                <button type="button" onClick={ submitCharacter }>Submit</button> 
+                <br/>
+                <br/>
+                <h1>List of Characters</h1>
+                { characterList.map((val, key) => { //the characterList should be an array here
+                  return (
+                    <div key = { key } className="characterList">
+                        <h3>Character Name: { val.CharacterName } </h3>
+                        <button onClick = {() => { deleteHandler(val.CharacterId)} }>DELETE</button>
+                        <br/>
+                        <input type="text" onChange = { (e) => { 
+                          setNewCharacterName(e.target.value) 
+                        }} placeholder = { val.CharacterName }></input> 
+                        {/* Note: val.CharacterId is from the GET, and this newCharacterName is for the update */}
+                        <button type="button" onClick = {() => { updateHandler(val.CharacterId, newCharacterName)}}>UPDATE</button>
+                      </div>  
+                      
+                  );
+                })}
+                
     </div>
   );
 }
